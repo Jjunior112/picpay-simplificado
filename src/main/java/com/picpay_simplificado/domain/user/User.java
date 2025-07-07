c5a1075bf1b6e68365ea6068e4345b5e791b5856
jjunior112 <juarezgcjr@gmail.com>
2025-07-07 13:06:00 -0300

    feat: create user entity

---
+++ src/main/java/com/picpay_simplificado/domain/user/User.java
@@ -0,0 +1,38 @@
+package com.picpay_simplificado.domain.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity(name = "users")
+@Table(name="users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of="id")
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstname;
+
+    private String lastName;
+
+    @Column(unique = true)
+    private String document;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private BigDecimal balance;
+
+    @Enumerated(EnumType.STRING)
+    private UserType userType;
+
+}
---
+++ src/main/java/com/picpay_simplificado/domain/user/UserType.java
@@ -0,0 +1,7 @@
+package com.picpay_simplificado.domain.user;
+
+public enum UserType {
+    COMMON,
+    MERCHANT
+
+}
